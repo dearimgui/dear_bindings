0da566114023fcfad1b4f87b2277471df1313ba4
Serhii Yolkin <serhii.yolkin@gmail.com>
2024-08-27 02:37:03 +0200

    Fixed potential public-facing API changing when function disambiguation is involved

--- dear_bindings.py
+++ dear_bindings.py
@@ -312,6 +312,18 @@ def convert_header(
                                                                  "_SDL_GameController"
                                                                  ])
 
+    if is_imgui_internal:
+        # Some functions in imgui_internal already have the Ex suffix,
+        # which wreaks havok on disambiguation
+        mod_rename_functions.apply(main_src_root, {
+            'ImGui_BeginMenuEx': 'ImGui_BeginMenuWithIcon',
+            'ImGui_MenuItemEx': 'ImGui_MenuItemWithIcon',
+            'ImGui_BeginTableEx': 'ImGui_BeginTableWithID',
+            'ImGui_ButtonEx': 'ImGui_ButtonWithFlags',
+            'ImGui_ImageButtonEx': 'ImGui_ImageButtonWithFlags',
+            'ImGui_InputTextEx': 'ImGui_InputTextWithHintAndSize',
+            'ImGui_RenderTextClippedEx': 'ImGui_RenderTextClippedWithDrawList',
+        })
 
     mod_disambiguate_functions.apply(dom_root,
                                      name_suffix_remaps={
@@ -445,42 +457,6 @@ def convert_header(
                                                           'flags',
                                                           'popup_flags'
                                                       ])
-        
-    if is_imgui_internal:
-        # Some functions in imgui_internal already have the Ex suffix,
-        # which wreaks havok on disambiguation
-        mod_rename_functions.apply(main_src_root, {
-            'ImGui_BeginMenuEx': 'ImGui_BeginMenuWithIcon',
-            'ImGui_MenuItemEx': 'ImGui_MenuItemWithIcon',
-            'ImGui_BeginTableEx': 'ImGui_BeginTableWithID',
-            'ImGui_ButtonEx': 'ImGui_ButtonWithFlags',
-            'ImGui_ImageButtonEx': 'ImGui_ImageButtonWithFlags',
-            'ImGui_InputTextEx': 'ImGui_InputTextWithHintAndSize',
-        })
-        
-    mod_disambiguate_functions.apply(dom_root,
-                                     name_suffix_remaps={
-                                         # Some more user-friendly suffixes for certain types
-                                         'const char*': 'Str',
-                                         'char*': 'Str',
-                                         'unsigned int': 'Uint',
-                                         'unsigned int*': 'UintPtr',
-                                         'ImGuiID': 'ID',
-                                         'const void*': 'Ptr',
-                                         'void*': 'Ptr'},
-                                     # Functions that look like they have name clashes but actually don't
-                                     # thanks to preprocessor conditionals
-                                     functions_to_ignore=[
-                                         "cImFileOpen",
-                                         "cImFileClose",
-                                         "cImFileGetSize",
-                                         "cImFileRead",
-                                         "cImFileWrite"],
-                                     functions_to_rename_everything=[
-                                         "ImGui_CheckboxFlags"  # This makes more sense as IntPtr/UIntPtr variants
-                                     ],
-                                     type_priorities={
-                                     })
 
     # Do some special-case renaming of functions
     mod_rename_functions.apply(dom_root, {
