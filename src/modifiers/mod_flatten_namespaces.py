4b6b58a4d07c0e0205c654ca38d8596cf0caff0b
Serhii Yolkin <serhii.yolkin@gmail.com>
2024-08-28 12:38:55 +0200

    Revert "Remove namespace from field types"
    This reverts commit b606455e94976e8123c30020ecc11578bdd98efc.

--- src/modifiers/mod_flatten_namespaces.py
+++ src/modifiers/mod_flatten_namespaces.py
@@ -1,13 +1,9 @@
 from src import code_dom
-from src import utils
-from typing import cast
 
 
 # This modifier removes namespaces and adds the name as a prefix to all the members inside
 # custom_mappings can be used to supply "namespace name -> prefix" mappings to use
 def apply(dom_root, custom_mappings=None):
-    all_fields = cast(list[code_dom.DOMFieldDeclaration], dom_root.list_all_children_of_type(code_dom.DOMFieldDeclaration))
-
     # Iterate through all namespaces
     for namespace in dom_root.list_all_children_of_type(code_dom.DOMNamespace):
 
@@ -28,16 +24,6 @@ def apply(dom_root, custom_mappings=None):
                 child.old_names = child.names.copy()
                 for i in range(0..len(child.names)):
                     child.names[i] = prefix + child.names[i]
-
-        # Remove namespace from field types. This is very shoddy...
-        for field in all_fields:
-            field_type = cast(code_dom.DOMType, field.field_type)
-            if len(field_type.tokens) < 4:
-                continue
-
-            field_type_name = field_type.get_fully_qualified_name()
-            field_type_name = field_type_name.replace(namespace.name + "::", "")
-            field_type.tokens = utils.create_tokens_for_type(field_type_name)
 
         # Remove the namespace element and promote the children into the parent scope
         children = namespace.children.copy()
