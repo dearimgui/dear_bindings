c9d7d482a25ceaeccb6db48b0456660580f46113
nmcintosh <nmcintosh@eus.v.co.zw>
2025-04-08 14:29:19 +0200

    sdlgpu3 bindings

---
+++ src/templates/imgui_impl_sdlgpu3-header-template.cpp
@@ -0,0 +1,12 @@
+#include "%IMGUI_INCLUDE_DIR%imgui.h"
+#include "%BACKEND_INCLUDE_DIR%imgui_impl_sdlgpu3.h"
+
+#include <stdio.h>
+
+// Wrap this in a namespace to keep it separate from the C++ API
+namespace cimgui
+{
+#include "%OUTPUT_HEADER_NAME%"
+}
+
+
